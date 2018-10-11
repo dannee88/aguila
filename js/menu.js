1106b193d7410456f9bbbffa04e19134f452ca19
dannee88 <danielmarquez421@gmail.com>
2018-10-10 20:49:26 -0700

    first commit

---
+++ js/menu.js
@@ -0,0 +1,236 @@
+const Appetizers = {
+	template:`<div class ='text-center'>
+          <h1>Appetizers</h1><br><br>
+
+          <h4>Tostada De Tinga</h4>
+          <img src="img/appetizers/tostada.jpg">
+          <p> All-natural chicken braised with caramelized onions 
+          	  and a splash of chipotle,<br> served on a crisp tortilla
+			  and finished up with romaine lettuce, queso fresco and mexican crema</p><br><br>
+
+			  <h4>Empanadas</h4>
+          <img src="img/appetizers/empanadas.jpg">
+          <p>2 fresh corn tortillas filled with calabacitas and potatoes,
+          	 Served with crema, <br> queso fresco and a beautiful
+			roasted tomato, cilantro and avocado sauce.</p><br><br>
+
+			<h4>Huarache</h4>
+          <img src="img/appetizers/huarache.jpg">
+          <p>Fresh handmade oblong corn tortilla,  layered with organic pinto 
+          beans refritos, guacamole,all-natural pulled beef<br> 
+          briskett, lettuce, queso fresco, avocado and paper thin red onions</p><br><br><br><br>
+        </div>
+	`
+}
+const Entrees = {
+	template:`<div class ='text-center'>
+          <h1>Entrees</h1><br><br>
+
+          <h4>Homestead Farms all-natural pork braised in Chile Verde</h4>
+          <img src="img/entrees/porkchile.jpg">
+          <p>Served with grilled tomatoes and Nopalitos, Spanish rice,organic whole black beans with epazote<br>and 2 fresh homemade corn tortillas (This Entree is also offered with all-natural chicken)
+
+ </p><br><br>
+
+			  <h4>Caldo Tlalpeno</h4>
+          <img src="img/entrees/caldo.jpg">
+          <p> Hearty all-natural chicken soup with calabacitas, blue lake beans,rice,<br> chayote,potatoes, carrots and a splash of chipotle.Finished up with fresh avocado and 2 tostadas</p><br><br>
+
+			  <h4>Oaxacan Style Mole</h4>
+          <img src="img/entrees/mole.jpg">
+          <p> All-natural chicken (leg and thigh) in our homemade oaxacan style Red Mole,
+		sprinkled with toasted sesame seeds.<br>Served with 2 fresh handmade corn tortillas, spanish rice and  organic whole black beans</p><br><br>
+
+			  <h4>Mole Verde</h4>
+          <img src="img/entrees/moleverde.jpg">
+          <p>All-natural chicken (leg and thigh) served with our most delicious sauce made
+with toasted ground pumpkin seeds . <br>Served with grilled calabacitas (mexican zucchine), spanish rice and 2 handmade corn tortillas</p><br><br>
+
+			  <h4>Chile Relleno</h4>
+          <img src="img/entrees/chilerelleno.jpg" width="500px" height="375px">
+          <p> Filled with Niman Ranch ground picadillo (beef) melted mozzarella cheese.Gluten free.</p><br><br>
+
+			  <h4>Tiger Prawn Salad All-Natural Chicken Salad</h4>
+          <img src="img/entrees/salad.jpg">
+          <p> Tiger Prawns or All-Natural chicken, fresh tomatoes, romaine lettuce,<br> watercress, radishes, avocado, in a cilantro and lemon vinaigrette</p><br><br>
+
+	
+			<h4>Tacos</h4>
+          <img src="img/entrees/tacos.jpg">
+          <p> Roasted Chicken, Al Pastor, Niman Ranch Beef asada, Homemade Chorizo, Pork  marinated
+			  in adobo <br> and slowly roasted, potatoes with caramelized onions and poblanos chiles,
+			  sauteed mushrooms, Tiger Prawns, nopalitos.<br> 
+			  Served with fresh onions and cilantro, lemon wedges, radishes and  house salsa</p><br><br>
+
+			  <h4>Burrito</h4>
+          <img src="img/entrees/burrito.jpg" width='500' height='375'>
+          <p> Roasted Chicken, Al Pastor, Niman Ranch Beef, Homemade Chorizo,
+		 Pork  marinated in adobo<br> Vegetarian, Tiger Prawns.Filled with spanish rice, organic whole black beans,
+		caramelized onions with tomatoes and jalapenos, julienne of lettuces,<br> 
+		roasted tomatillos and chile de arbol sauce and California Gold mozzarella.
+		served with a side house potato chips.</p><br><br>
+
+		<h4>Quesadilla</h4>
+          <img src="img/entrees/quesadilla.jpg">
+          <p> Lightly toasted handmade Corn Tortilla served with California Girl mozzarella,
+			mexican crema, queso fresco,julienne of lettuces, salsa and guacamole.<br> Your choice of: 
+			-Mozzarella and epazote -Mushrooms -Potatoes and chiles poblanos -Chicken Tinga -Niman Ranch Beef
+			-Tiger Prawns -Chorizo -Pastor -Pork in adobo.</p><br><br>
+
+		<h4>Pambazo</h4>
+          <img src="img/entrees/pambazo.jpg">
+          <p>Fresh Telera bread brushed over with Guajillo Chile Sauce,
+			filled with Homemade Chorizo, caramelized onions and potatoes.<br>Lightly crisped on griddle.
+			Finished up with, julienne of lettuces, queso fresco and a drizzle of mexican crema</p><br><br>
+
+		<h4>Torta</h4>
+          <img src="img/entrees/torta.jpg">
+
+          <p> All-Natural Chicken, Al Pastor, Niman Ranch Beef asada Homemade Chorizo Pork marinated in adobo, sauteed mushrooms, potatoes and poblanos chiles,<br> Tiger Prawns.Lightly toasted sandwich made using fresh mexican "Telera" bread,filled with julienne of romaine lettuce,<br> sliced Tomatoes, Jalapenos, Onions, California Girl mozzarella, Avocado, touch of mayo. served with a side house potato chips.
+			</p><br><br><br><br><br>
+
+		</div>
+	`
+}
+const Drinks = {
+	template:`<div class ='text-center'>
+          <h1>Alcoholic Drinks</h1><br><br>
+
+          <h4>Aguila Mojito</h4>
+          <img src="img/drinks/mojito.jpg" width='500' height='375'><br><br><br>
+
+			<h4>Pomegranite Agave Margarita</h4>
+          <img src="img/drinks/pomagave.jpg" width='500' height='375'><br><br><br>
+
+			<h4>Tamarindo Agave Margarita</h4>
+          <img src="img/drinks/tamarindoagave.jpg"><br><br><br>
+
+          <h1>Beer</h1>
+          <h5>Pacifico<br> 
+          Corona <br> 
+          Negra Modelo <br>
+          Lagunitas IPA</h5>
+          <br>
+
+          <h1>Wine</h1>
+          <h5>Malbec, Portillo,2014, Uco Valley, Mendoza, Argentina<br> 
+          Brut Reserva Cava,Segura Viudas, Spain. individual mini bottle <br> 
+          Chardonnay, LaTour Ardeche 2014, Burgundy, France <br>
+          </h5>
+
+          <h1>Non-Alcoholic Drinks</h1><br>
+          	<h3>All Natural Homemade Aguas Frescas</h3>
+          		
+          		<h5>
+          		Horchata, rice drink with almond milk, vanilla and cinnamon<br> 
+          		Jamaica (Hibiscus Iced Tea) <br> 
+          		seasonal fresh fruit agua fresca<br>
+          		Specialty Sodas (Jarritos)<br>
+          		Bottle coke/diet coke<br>
+          		Bottled Water
+
+ 
+          		</h5>
+          		<h3>Made to Order Juices</h3>
+          		<h5>
+          		Carrots<br> 
+          		Orange small or large<br> 
+          		</h5>
+
+          		<h2>Coffees</h2>
+          		Americano<br> 
+          		Espresso <br> 
+          		Cappuccino<br>
+          		CaffeLatte<br>
+          		Mexican Hot Chocolate<br><br><br><br>
+          		
+
+       </div>
+	`
+}
+const Sides = {
+	template:`<div class ='text-center'>
+          <h1>Sides</h1><br><br>
+          <img src="img/sides/rice.jpg" width="500px" height="375px"><br><br><br>
+          <p>
+
+          	-Spanish Rice $3.25<br><br>
+
+          	-Side Soup of the Day $8<br><br>
+
+            -Side green salad $7<br><br>
+
+			-Organic Black Beans $ 3.95<br><br>
+
+			-Spanish Rice $3.25<br><br>
+
+			-Nopalitos tender cactus leaves served with tomatoes, red onions and queso fresco $4.25<br><br>
+
+			- Homemade Potato chips and salsa $2.50<br><br>
+
+			-Fresh handmade corn tortilla $ 50 cents ea</p><br><br><br><br>
+        </div>
+	`
+}
+const Desserts = {
+	template:`<div class ='text-center'>
+          <h1>Desserts</h1><br><br>
+
+
+          <h4>Arroz con Leche</h4>
+          <img src="img/desserts/arroz.jpg" width="500px" height="375px"><br><br><br><br>
+
+			  <h4>Cheesecake</h4>
+          <img src="img/desserts/cheesecake.jpg"><br><br><br><br>
+
+
+			<h4>Mexican Wedding Cookies</h4>
+          <img src="img/desserts/cookies.jpg"><br><br><br><br>
+         
+
+          <h4>Flan</h4>
+          <img src="img/desserts/flan.jpg" width="500px" height="375"><br><br><br><br>
+          
+
+        </div>
+	`
+}
+
+const router = new VueRouter ({
+	routes:[
+	{
+		path: '/Appetizers',
+		component: Appetizers
+	},
+	{
+		path: '/Entrees',
+		component: Entrees
+	},
+	{
+		path:'/Drinks',
+		component:Drinks
+	},
+	{
+		path:'/Sides',
+		component: Sides
+	},
+	{
+		path:'/Desserts',
+		component: Desserts
+	},
+		]
+})
+
+var whips = new Vue ({
+
+	router,
+	el: '#app',
+	data: {
+
+	},
+	methods:{
+
+	} 
+
+})
+
